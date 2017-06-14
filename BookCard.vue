828bb758084b8e45dbbc682d337c458e7964e428
Robin, Lu <luhaibin731@gmail.com>
2017-06-14 18:21:22 +0800

    Add files via upload

---
+++ BookCard.vue
@@ -0,0 +1,52 @@
+<template >
+<div class="container">
+
+    <div class="col-sm-6 col-md-4" v-for="book in books">
+    <div class="thumbnail">
+      <img data-src="holder.js/300x300" alt="...">
+      <div class="caption">
+       <div id="BookCard">
+       </div>
+        <h3>{{book.title}}</h3>
+        <p> <span class="label label-primary"> </span></p>     
+   <p><button type="button" class="btn btn-success">Purchase</button></p>
+   <div v-if="auth">
+       <p><a :href="'books/edit/'+ book.id" class="btn btn-info" role="button">Edit</a>
+<a :href="'books/delete/'+  book.id " class="btn btn-info" role="button">Delete</a>
+        </p>
+   </div>
+   <p> <span class="label label-primary"> {{book.tech_field}} </span></p>
+
+   <div class="progress">
+  <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" :style="{width:book.progress + '%'}">
+ {{book.progress}}%
+  </div>
+</div>
+   </div>
+   </div>
+   </div>
+</div>
+
+</template>
+
+<script>
+    export default {
+      
+        mounted() {
+       console.log('Component be mounted!')
+        },
+        props:{
+            books: Array,
+            auth: Object,
+        },
+        data:function () {
+          return {
+            progress:{width:book.progress}
+          }
+        }
+     
+
+
+
+    }
+</script>
